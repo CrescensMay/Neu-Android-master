000474c07d20ed28d03b7afea95f696d84c05693
Fidele <crescens.kob@outlook.fr>
2017-07-13 10:09:11 +0800

    Created anorther activity called HomeActivity so that you can interact with another Page by clicking a simple button

---
+++ chuck/src/main/java/com/neuandroid/chuck/HomeActivity.java
@@ -0,0 +1,33 @@
+package com.neuandroid.chuck;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class HomeActivity extends Activity{
+
+    private Button jButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+
+        jButton = (Button) findViewById(R.id.go_to_joke_page);
+
+        jButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
